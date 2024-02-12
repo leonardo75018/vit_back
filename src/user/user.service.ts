6cec570b4fd8a297ce1f7732198102cb9973f36d
Kabongo <leonardo.kabongo@gmail.com>
2024-02-12 21:06:07 +0100

    create user-service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private prismaService: PrismaService) {}
+  async create(createUserDto: CreateUserDto) {
+    const user = await this.prismaService.user.findUnique({
+      where: { email: createUserDto.email },
+    });
+
+    if (user) {
+      throw new Error('User already exists');
+    }
+    return this.prismaService.user.create({ data: createUserDto });
   }
 
   findAll() {
-    return `This action returns all user`;
+    return this.prismaService.user.findMany();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    return this.prismaService.user.findUnique({ where: { id } });
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+    return this.prismaService.user.update({
+      where: { id },
+      data: updateUserDto,
+    });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} user`;
+    return this.prismaService.user.delete({ where: { id } });
   }
 }
